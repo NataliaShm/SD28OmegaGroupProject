a6f7a9d3342b99a9cf3f7aff1f0302d829c302d6
Tracy59 <tracy.mccormack@RobertsonCollege.net>
2018-06-05 15:56:55 -0500

    Created models.Hotels.ds

---
+++ GroupProject/GroupProject/Models/Hotels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Hotels
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+        public string website { get; set; }
+        public string description { get; set; }
+
+        // Navigation property
+        // public ICollection<Accomodation> Accomodations { get;set;}
+
+    }
+}
